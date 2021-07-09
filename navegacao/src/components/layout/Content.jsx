15db3169a334eccb57f89d8afa39a126ed6d7f46
Gustavo Rosano Teixeira <gurosano@gmail.com>
2021-07-08 21:24:00 -0300

    adicionei a pasta src

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,29 @@
+import './Content.css'
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Home from '../../views/exemples/Home'
+import Param from '../../views/exemples/Param'
+import About from '../../views/exemples/About'
+import NotFound from '../../views/exemples/NotFound'
+
+const Content = props => (
+  <main className="Content">
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/param/:id">
+        <Param />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  </main>
+)
+
+export default Content
